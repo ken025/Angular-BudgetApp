491ec76b0f15eee8719c0fe922a218b214d1c79f
ken025 <kennethmanuel250@gmail.com>
2021-05-03 22:06:36 -0400

    added cards for income and expenses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddFormComponent } from './add-form/add-form.component';
 import { BudgetItemLisyComponent } from './budget-item-lisy/budget-item-lisy.component';
+import { BudgetItemCardComponent } from './budget-item-lisy/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     AddFormComponent,
-    BudgetItemLisyComponent
+    BudgetItemLisyComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
